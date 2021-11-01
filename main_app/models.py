2d069ff9a150fe241a9e420d2373b9cc29d35aed
Adam Millar <adammillar@Adams-Air.hsd1.wa.comcast.net>
2021-11-01 16:52:27 -0700

    Lab P2: Add index and detail page. Admin functionality. Finches database

--- main_app/models.py
+++ main_app/models.py
@@ -5,3 +5,5 @@ class Finch(models.Model):
     name = models.CharField(max_length=100)
     beaklength= models.CharField(max_length=100)
     age = models.IntegerField()
+    def __str__(self):
+        return self.name
