c645cd9f8c8b2f12b53eff83964a07954b0b8e39
Adam Millar <adammillar@Adams-Air.hsd1.wa.comcast.net>
2021-11-02 15:22:27 -0700

    Begin 1:M relationship with Feeding model. Few routing bug fixes

--- main_app/models.py
+++ main_app/models.py
@@ -16,8 +16,15 @@ class Finch(models.Model):
     def __str__(self):
         return self.name
     def get_absolute_url(self):
-        return reverse('detail', kwargs={'cat_id': self.id})
+        return reverse('detail', kwargs={'finch_id': self.id})
 
 class Feeding(models.Model):
-    date = models.DateField()
-    meal = models.CharField(max_length=1)
+    date = models.DateField('feeding date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0],
+    )
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
