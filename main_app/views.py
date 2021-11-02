79399ef204111d32002a990533e69687972c040b
Adam Millar <adammillar@Adams-Air.hsd1.wa.comcast.net>
2021-11-01 19:57:42 -0700

    Begin Lab P3

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
+from django.db.models import fields
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from .models import Finch
 
 # class Finch: 
@@ -27,3 +29,9 @@ def finches_index(request):
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
     return render(request, 'finches/detail.html', { 'finch': finch})
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+
+
